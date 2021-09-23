8c2c1e7226ac425b7fe49ae5e76f80fbfd929e27
Manindri <IT20246778@my.sliit.lk>
2021-09-24 01:32:09 +0530

    modified interfaces

--- src/goals/app/src/main/java/com/example/newfinanceapp/updateGoal.java
+++ src/goals/app/src/main/java/com/example/newfinanceapp/updateGoal.java
@@ -14,22 +14,22 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class updateGoalActivity extends AppCompatActivity {
+public class updateGoal extends AppCompatActivity {
 
-    EditText goal_input, amount_input, desc_input;
+    EditText name_input, amount_input, description_input;
     Button update_button;
     ImageButton back_button2 , delete_button;
 
-    String id, goal, amount, desc;
+    String id, name, amount, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_income);
+        setContentView(R.layout.activity_update_goal);
 
-        goal_input = findViewById(R.id.updateField1_text);
+        name_input = findViewById(R.id.updateField1_text);
         amount_input = findViewById(R.id.updateField2_text);
-        desc_input = findViewById(R.id.updateField3_text);
+        description_input = findViewById(R.id.updateField3_text);
         update_button = findViewById(R.id.update_button);
         back_button2 = findViewById(R.id.back_button2);
         delete_button = findViewById(R.id.delete_button);
@@ -47,11 +47,11 @@ public class updateGoalActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //And only then we call this
-                MyDatabaseHelper myDB = new MyDatabaseHelper(updateGoalActivity.this);
-                goal = goal_input.getText().toString().trim();
+                MyDatabaseHelper myDB = new MyDatabaseHelper(updateGoal.this);
+                name = name_input.getText().toString().trim();
                 amount = amount_input.getText().toString().trim();
-                desc = desc_input.getText().toString().trim();
-                myDB.updateData(id, goal, amount, desc);
+                description = description_input.getText().toString().trim();
+                myDB.updateData(id, name, amount, description);
             }
         });
         delete_button.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +64,124 @@ public class updateGoalActivity extends AppCompatActivity {
         back_button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (updateGoalActivity.this, MainActivity.class);
+                Intent intent = new Intent (updateGoal.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    void getAndSetIntentData(){
+        if(getIntent().hasExtra("id") && getIntent().hasExtra("name") &&
+                getIntent().hasExtra("amount") && getIntent().hasExtra("description")){
+            //Getting Data from Intent
+            id = getIntent().getStringExtra("id");
+            name = getIntent().getStringExtra("name");
+            amount = getIntent().getStringExtra("amount");
+            description = getIntent().getStringExtra("description");
+
+            //Setting Intent Data
+            name_input.setText(name);
+            amount_input.setText(amount);
+            description_input.setText(description);
+            Log.d("stev", name+" "+amount+" "+description);
+        }else{
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete " + name + " ?");
+        builder.setMessage("Are you sure you want to delete " + name + " ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(updateGoal.this);
+                myDB.deleteOneRow(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
+    }
+}
+
+/*
+package com.example.newfinanceapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class updateGoal extends AppCompatActivity {
+
+    EditText goal_input, amount_input, desc_input;
+    Button update_button;
+    ImageButton back_button2 , delete_button;
+
+    String id, goal, amount, description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_goal);
+
+        goal_input = findViewById(R.id.updateField1_text);
+        amount_input = findViewById(R.id.updateField2_text);
+        desc_input = findViewById(R.id.updateField3_text);
+        update_button = findViewById(R.id.update_button);
+        back_button2 = findViewById(R.id.back_button2);
+        delete_button = findViewById(R.id.delete_button);
+
+        //First we call this
+        getAndSetIntentData();
+
+        //Set actionbar title after getAndSetIntentData method
+        /*ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setTitle(title);
+        }*/
+/*
+        update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //And only then we call this
+                MyDatabaseHelper myDB = new MyDatabaseHelper(updateGoal.this);
+                goal = goal_input.getText().toString().trim();
+                amount = amount_input.getText().toString().trim();
+                description = desc_input.getText().toString().trim();
+                myDB.updateData(id, goal, amount, description);
+            }
+        });
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog();
+            }
+        });
+
+        back_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (updateGoal.this, MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -73,18 +190,18 @@ public class updateGoalActivity extends AppCompatActivity {
 
     void getAndSetIntentData(){
         if(getIntent().hasExtra("id") && getIntent().hasExtra("goal") &&
-                getIntent().hasExtra("amount") && getIntent().hasExtra("desc")){
+                getIntent().hasExtra("amount") && getIntent().hasExtra("description")){
             //Getting Data from Intent
             id = getIntent().getStringExtra("id");
-            note = getIntent().getStringExtra("goal");
+            goal = getIntent().getStringExtra("goal");
             amount = getIntent().getStringExtra("amount");
-            category = getIntent().getStringExtra("desc");
+            description = getIntent().getStringExtra("description");
 
             //Setting Intent Data
-            note_input.setText(goal);
+            goal_input.setText(goal);
             amount_input.setText(amount);
-            category_input.setText(desc);
-            Log.d("stev", goal+" "+amount+" "+desc);
+            desc_input.setText(description);
+            Log.d("stev", goal+" "+amount+" "+description);
         }else{
             Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
         }
@@ -99,7 +216,7 @@ public class updateGoalActivity extends AppCompatActivity {
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                MyDatabaseHelper myDB = new MyDatabaseHelper(updateGoalActivity.this);
+                MyDatabaseHelper myDB = new MyDatabaseHelper(updateGoal.this);
                 myDB.deleteOneRow(id);
                 finish();
             }
@@ -112,4 +229,4 @@ public class updateGoalActivity extends AppCompatActivity {
         });
         builder.create().show();
     }
-}
+}*/
