ec9eed02e96e9af1077af77516f430241071eed8
Manindri <IT20246778@my.sliit.lk>
2021-09-25 11:11:27 +0530

    added validatins for goals function

--- src/goals/app/src/main/java/com/example/newfinanceapp/addActivity.java
+++ src/goals/app/src/main/java/com/example/newfinanceapp/addActivity.java
@@ -8,13 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
 
 public class addActivity extends AppCompatActivity {
 
+    //Initialize Variables
     EditText goalNameInput, goalAmountInput, goalDesInput;
     Button add_button;
     ImageButton back_button;
@@ -23,29 +23,50 @@ public class addActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        //Assign Variables
         goalNameInput = findViewById(R.id.addField1_text);
         goalAmountInput = findViewById(R.id.addField2_text);
         goalDesInput = findViewById(R.id.addFeild_text3);
         add_button = findViewById(R.id.add_button);
         back_button = findViewById((R.id.back_button));
 
-        MyDatabaseHelper myDB = new MyDatabaseHelper(addActivity.this);
         add_button.setOnClickListener(new View.OnClickListener(){
+
+            MyDatabaseHelper myDB = new MyDatabaseHelper(addActivity.this);
 
             @Override
             public void onClick(View view) {
 
-                Boolean insert = myDB.addGoal (goalNameInput.getText().toString().trim(),
-                        goalAmountInput.getText().toString().trim(),
-                        goalDesInput.getText().toString().trim());
+                String name = goalNameInput.getText().toString();
+                String amount = goalAmountInput.getText().toString();
+                String description = goalDesInput.getText().toString();
 
-                if(insert==true){
-                    Toast.makeText(addActivity.this, "Inserted Successfully" , Toast.LENGTH_SHORT).show();
+                //making a function for validation and pass all parameters
+                boolean  check= validateinfo(name,amount,description);
+
+                if (check == true) {
+
+                    //when data are in valid formats, input data to the databaase
+                    Boolean insert = myDB.addGoal (goalNameInput.getText().toString().trim(),
+                            goalAmountInput.getText().toString().trim(),
+                            goalDesInput.getText().toString().trim());
+
+                    if(insert==true){
+                        Toast.makeText(addActivity.this, "Inserted Successfully" , Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(addActivity.this, "Error!!" , Toast.LENGTH_SHORT).show();
+                    }
+
+                    Toast.makeText(getApplicationContext(), "Data is valid",Toast.LENGTH_SHORT).show();
                 }
-                else{
-                    Toast.makeText(addActivity.this, "Error!!" , Toast.LENGTH_SHORT).show();
+                else {
+                    Toast.makeText(getApplicationContext(),"Sorry check information again",Toast.LENGTH_SHORT).show();
                 }
+
+
             }
+
         });
 
          back_button.setOnClickListener(new View.OnClickListener() {
@@ -83,5 +104,37 @@ public class addActivity extends AppCompatActivity {
         });
 
         title.setText("Manage Goals");
+    }
+
+    //validation
+    private boolean validateinfo(String name, String amount, String description) {
+        if (name.length() == 0) {
+            goalNameInput.requestFocus();
+            goalNameInput.setError("THIS FIELD CAN NOT BE EMPTY");
+            return false;
+        } else if (!name.matches("^\\s*[\\da-zA-Z][\\da-zA-Z\\s]*$")) {
+            goalNameInput.requestFocus();
+            goalNameInput.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+            return false;
+        } else if (amount.length() == 0) {
+            goalAmountInput.requestFocus();
+            goalAmountInput.setError("FIELD CAN NOT BE EMPTY");
+            return false;
+        } else if (!amount.matches("\\d+")) {
+            goalAmountInput.requestFocus();
+            goalAmountInput.setError("PLEASE ENTER NUMBERS");
+            return false;
+        } else if (description.length() == 0) {
+            goalDesInput.requestFocus();
+            goalDesInput.setError("FILED CAN NOT BE EMPTY");
+            return false;
+        } else if (!name.matches("^\\s*[\\da-zA-Z][\\da-zA-Z\\s]*$")) {
+        goalNameInput.requestFocus();
+        goalNameInput.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
