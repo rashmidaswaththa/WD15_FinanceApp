de959dc7ef63d880238946c6b982c03d324931ff
kaviperamuna <it20237004@my.sliit.lk>
2021-09-25 18:30:45 +0530

    adding validations to update reminders

--- src/all/app/src/main/java/com/example/newfinanceapp/addRemActivity.java
+++ src/all/app/src/main/java/com/example/newfinanceapp/addRemActivity.java
@@ -35,16 +35,34 @@ public class addRemActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Boolean insert = myDB.addReminder (Rtype_input.getText().toString().trim(),
-                        Ramount_input.getText().toString().trim(),
-                        Rdate_input.getText().toString().trim());
+                String type = Rtype_input.getText().toString();
+                String amount = Ramount_input.getText().toString();
+                String date = Rdate_input.getText().toString();
+
+                //making a function for validation and pass all parameters
+                boolean  check= validateinfo(type,amount,date);
+
+                if(check==true){
+
+                    //When data are in valid formats, input data to the database
+                    Boolean insert = myDB.addReminder (Rtype_input.getText().toString().trim(),
+                            Ramount_input.getText().toString().trim(),
+                            Rdate_input.getText().toString().trim());
 
 
-                if(insert==true){
+
+
+                    if(insert==true){
                     Toast.makeText(addRemActivity.this, "Inserted Successfully" , Toast.LENGTH_SHORT).show();
                 }
                 else{
                     Toast.makeText(addRemActivity.this, "Error!!" , Toast.LENGTH_SHORT).show();
+                }
+                    Toast.makeText(getApplicationContext(), "Data is valid",Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(),"Sorry check information again",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -76,5 +94,46 @@ public class addRemActivity extends AppCompatActivity {
         });
 
         title.setText("Manage Reminder");
+    }
+
+    //validation method
+    private boolean validateinfo(String type, String amount, String date) {
+
+        if (type.length() == 0) {
+            //Checking for null type inputs
+            Rtype_input.requestFocus();
+            Rtype_input.setError("THIS FIELD CAN NOT BE EMPTY");
+            return false;
+        } else if (!type.matches("^\\s*[\\da-zA-Z][\\da-zA-Z\\s]*$")) {
+            //Checking for relevant input types for the fields
+            Rtype_input.requestFocus();
+            Rtype_input.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+            return false;
+        } else if (amount.length() == 0) {
+            //Checking for null amount inputs
+            Ramount_input.requestFocus();
+            Ramount_input.setError("FIELD CAN NOT BE EMPTY");
+            return false;
+        } else if (!amount.matches("\\d+")) {
+            //Checking for relevant input types for the fields
+            Ramount_input.requestFocus();
+            Ramount_input.setError("PLEASE ENTER NUMBERS");
+            return false;
+        } else if (date.length() == 0) {
+            //Checking for null amount inputs
+            Rdate_input.requestFocus();
+            Rdate_input.setError("FIELD CAN NOT BE EMPTY");
+            return false;
+        } else if (!date.matches("^(0[1-9]|[12][0-9]|3[01])\\/(0[1-9]|1[0-2])\\/([12][0-9]{3})$")){
+            //Checking for relevant input types for the fields
+            Rdate_input.requestFocus();
+            Rdate_input.setError("PLEASE ENTER IN DD/MM/YYYY FORMAT");
+            return false;
+        }
+        else {
+            return true;
+        }
+
+
     }
 }
